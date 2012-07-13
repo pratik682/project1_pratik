673b6410ed9ae59a31444e6540c1da6e28683fea
pratik682 <pratik682_pkc@yahoo.com>
2012-07-13 11:51:24 +0530

    THE RAILS ASSIGNMENT

---
+++ demo_app_pkc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppPkc::Application.config.secret_token = '301aa26f468235fec2fcbca299681c2653a6e1cd7211d5378520fc7aca9804e679c5cf6e650820eb7ae973c40b7f66964b5d8c178347392ab05eb7ff378dfcce'
