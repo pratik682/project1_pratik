5d8b51b3a7e9d0af5b5040fc52a6b3147d658b45
pratik682 <pratik682_pkc@yahoo.com>
2012-07-15 19:12:43 +0530

    Final Rails Application

---
+++ demo_app_pratik/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+def index
+  @users = User.all
+end
+def show
+  @user = User.find(params[:id])
+end
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect_to users_path 
+end
+def new
+  @user = User.new
+end
+def create
+  @user = User.new(params[:user])
+  @user.save
+  redirect_to users_path
+end
+def edit
+  @user = User.find(params[:id])
+end
+def update
+  @user = User.find(params[:id])
+  @user.update_attributes(params[:user])
+  redirect_to users_path
+end
+end
---
+++ demo_app_pratik/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppPratik::Application.config.secret_token = '17f7af1f352c0430940d85eba2cb890d5aa2c2c32456c5855762f8e4b44739c522dc624ae23eee8a1f343f8a9559f9dd467c976b1d669aeabf7113742b8d7c51'
